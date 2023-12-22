9befaa7ea0e441cc7a6475e3770a307dbdcd2ed2
Phoenix <Phoenixn82@gmail.com>
2023-12-22 12:37:28 -0600

    Added nav functionality, clock

--- components/nav.php
+++ components/nav.php
@@ -1,19 +1,19 @@
  <section class="mac-menu">
     <section class="nav-container">
       <div class="nav-left">
-        <ul class="links">
-          <li class="icon-wrapper"><a href="#"></a></li>
-          <li class="icon-wrapper"><a href="#"></a></li>
-          <li class="icon-wrapper"><a href="#"></a></li>
-          <li class="icon-wrapper"><a href="#"></a></li>
+          <div class="icon-wrapper"><?php include('images/svg/apple-logo.svg'); ?></div>
+          <div class="icon-wrapper"><a href="#">Linkdin</a></div>
+          <div class="icon-wrapper"><a href="#">Github</a></div>
+          <div class="icon-wrapper"><a href="#">Substack</a></div>
       </div>
       <div class="nav-right">
-        <ul class="links">
-          <li class="icon-wrapper"><a href="#"></a></li>
-          <li class="icon-wrapper"><a href="#"></a></li>
-          <li class="icon-wrapper"><a href="#"></a></li>
-          <li class="icon-wrapper"><a href="#"></a></li>
+          <div class="icon-wrapper"><a href="#"></a></div>
+          <div class="icon-wrapper"><a href="#"></a></div>
+          <div class="icon-wrapper"><a href="#"></a></div>
+          <div class="icon-wrapper"><p id="current-time"></p></div>
         </ul>
       </div>
     </section>
   </section>
+
+  <script src="js/clock.js"></script>
