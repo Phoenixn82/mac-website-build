0b218a13efa4bec2583b4e72c6e788f221d6fdeb
Phoenix <Phoenixn82@gmail.com>
2024-04-26 16:18:04 -0500

    Added Notes App

--- components/apps/portfolio.php
+++ components/apps/portfolio.php
@@ -1,1 +1,118 @@
-portfolio.php
+<section class="container-wrapper">
+  <section class="container">
+    <div class="item item-1" >1</div>
+    <div class="item item-2" >2</div>
+    <div class="item item-3" >3</div>
+    <div class="item item-4" >
+      <p class="notes-list-header">On My Mac</p>
+        <ul class="notes-left-list">
+          <li>
+            <div>
+              <img class="" src="images/svg/small-icons/folder.svg" alt="">
+              <p>General</p>
+             </div>
+            <p>8</p>
+          </li>
+          <li>
+            <div>
+              <img class="" src="images/svg/small-icons/book.svg" alt="">
+              <p>Books</p>
+             </div>
+            <p>3</p>
+          </li>
+          <li>
+            <div>
+              <img class="" src="images/svg/small-icons/file.svg" alt="">
+              <p>Drafts</p>
+             </div>
+            <p>0</p>
+          </li>
+          <li>
+            <div>
+              <img class="" src="images/svg/small-icons/tray.svg" alt="">
+              <p>Random</p>
+             </div>
+            <p>5</p>
+          </li>
+          <li>
+            <div>
+              <img class="" src="images/svg/small-icons/flag.svg" alt="">
+              <p>Work</p>
+             </div>
+            <p>1</p>
+          </li>    
+        </ul>
+      <p class="notes-list-header">iCloud</p>
+       <ul class="notes-left-list">
+          <li>
+            <div>
+              <img class="" src="images/svg/small-icons/pages.svg" alt="">
+              <p>Planning</p>
+             </div>
+            <p>6</p>
+          </li>
+          <li>
+            <div>
+              <img class="" src="images/svg/small-icons/list.svg" alt="">
+              <p>Workouts</p>
+             </div>
+            <p>9</p>
+          </li>
+        </ul>
+     </div>
+    <div class="item item-5" >
+      <ul class="notes-project-list">
+          <li>
+            <h2>Figma Project</h2>
+             <div class="project-list-time-desc">
+              <p>12:35 PM</p>
+              <p>Desinged a product that works</p>                
+             </div>
+          </li>
+          <li>
+            <h2>Duolingo Project</h2>
+             <div class="project-list-time-desc">
+              <p>Yesterday, 9:02 AM</p>
+              <p>Finished working on </p>                
+             </div>
+          </li>
+          <li>
+            <h2>Personal Website</h2>
+             <div class="project-list-time-desc">
+              <p>Last week, 9:02AM</p>
+              <p>Start the day off </p>                
+             </div>
+          </li>
+          <li>
+            <h2>Figma Project</h2>
+             <div class="project-list-time-desc">
+              <p>Last month, 2:26 PM</p>
+              <p>Redesign the</p>                
+             </div>
+          </li>
+          <li>
+            <h2>Figma Project</h2>
+             <div class="project-list-time-desc">
+              <p>2 months ago, 6:23 AM</p>
+              <p>Desinged a product that w...</p>                
+             </div>
+          </li>
+        </ul>
+    </div>
+    <div class="item item-6" >
+      <p class="project-time")>21 December at 12:35 PM</p>
+      <h1>Project Name</h1> 
+      <p>Project description...Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <ul>
+        <li><p>Things that I Learned</p></li>
+        <li><p>Things that I Learned</p></li>
+        <li><p>Things that I Learned</p></li>
+        <li><p>Things that I Learned</p></li>
+        <li><p>Things that I Learned</p></li>
+        <li><p>Things that I Learned</p></li>
+      </ul>
+    </div>
+
+  </section>
+</section>
+<script src="js/apps/portfolio.js"></script>
