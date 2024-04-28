cc677241f7e6f4184d0b55872f69e7e8f999ed24
Phoenix <Phoenixn82@gmail.com>
2024-04-27 21:08:21 -0500

    Images Added

---
+++ js/apps/portfolio.js
@@ -0,0 +1,27 @@
+// Get all li elements with class item-4
+const item4ListItems = document.querySelectorAll('.item-4 li');
+const item5ListItems = document.querySelectorAll('.item-5 li');
+
+
+// Add click event listener to each li element
+item4ListItems.forEach(li => {
+  li.addEventListener('click', () => {
+    // Remove selected class from all li elements
+    item4ListItems.forEach(otherLi => {
+      otherLi.classList.remove('selected');
+    });
+    // Add selected class to the clicked li element
+    li.classList.add('selected');
+  });
+});
+// Add click event listener to each li element under item-5
+item5ListItems.forEach(li => {
+  li.addEventListener('click', () => {
+    // Remove selected class from all li elements under item-5
+    item5ListItems.forEach(otherLi => {
+      otherLi.classList.remove('selected');
+    });
+    // Add selected class to the clicked li element
+    li.classList.add('selected');
+  });
+});
---
+++ small-components/min-max-buttons.php
@@ -0,0 +1,5 @@
+<div class="min-max-wrapper">
+  <div class="btn close-btn" onclick="toggleDiv('div-5')"></div>
+  <div class="btn min-btn" onclick="toggleDiv('div-5')"></div>
+  <div class="btn max-btn"></div>
+</div>
